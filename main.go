fdcb66c40523ca7279ca29df2edb1317fea3d0f9
elkofy <nassimdaikh71@gmail.com>
2026-01-25 16:05:15 +0100

    feat : cleaned up price

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"example/techsheets-api/database"
 	"example/techsheets-api/pkg"
 	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/xuri/excelize/v2"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"log"
-	"net/http"
 )
 
 func addIngredient(c *gin.Context) {
@@ -51,7 +55,32 @@ func getAllIngredients(c *gin.Context) {
 	c.JSON(http.StatusOK, ingredients)
 }
 
-func cellIngredientParser(s *string) {
+func cellIngredientParser(s *string, price *string) {
+	fmt.Println(*s)
+	cleanedStr := strings.ReplaceAll(*s, " ", "")
+	pattern := regexp.MustCompile(`(?mi)(\d{1,}G+)|(\d{1,}L)|(\d{1,}KG)|(\d{0,}X\d{1,})|(C\/\d{0,})|\d{1,}CL|(\d{1,}ML)|(\d{1,}G)`)
+	matches := pattern.FindAllString(cleanedStr, -1)
+	// fmt.Println(len(matches))
+	if len(matches) == 1 {
+		digitsreg := regexp.MustCompile(`(?mi)\d{1,}`)
+		digits := digitsreg.FindAllString(matches[0], -1)
+		unit := strings.ReplaceAll(matches[0], digits[0], "")
+		
+		clnPrice := strings.ReplaceAll(*price, " ", "")
+		clnPrice = strings.ReplaceAll(clnPrice,"€", "")
+		fmt.Println("quantity", digits[0])
+		fmt.Println("unit", unit)
+		fmt.Println("price", clnPrice)
+		iQuantity, err := strconv.ParseFloat(digits[0],64)
+		iPrice, err2 := strconv.ParseFloat(clnPrice,64)
+		unitPrice := iPrice/iQuantity
+		if err==nil && err2 == err{
+			formatUP := strconv.FormatFloat(unitPrice, 'E', -1, 64)
+			println("formatUP",formatUP)
+		}
+
+	}
+	// (\d{1,}G+)|(\d{1,}L)|(\d{1,}KG)
 
 }
 
@@ -69,14 +98,14 @@ func iterateOverSheets(f *excelize.File) {
 				fmt.Println(err)
 			}
 			for key, colCell := range row {
-				fmt.Print(key, "\t")
-				if ( key == 1){
-					
+				// fmt.Print(key, "\t")
+				if key == 1 {
+					fmt.Print(row[3])
+					cellIngredientParser(&colCell, &row[4])
 				}
-				cellIngredientParser(&colCell)
-				fmt.Print(colCell, "\t")
+				// fmt.Print(colCell, "\t")
 			}
-			fmt.Println()
+			// fmt.Println()
 		}
 		if err = rows.Close(); err != nil {
 			fmt.Println(err)
